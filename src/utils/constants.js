6637cef4e8a16b0dd189310ebb130c8c8028d1b5
Olga Ilyina <kukushkina@yandex.ru>
2021-05-26 14:51:50 +0300

    изменения url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://nomoreparties.co/news/v2/everything?language=ru&apiKey=71fe7273cc9c48ddbfeab0fd939e9571&pageSize=100&sortBy=relevancy';
-export const  BASE_URL_AUTH = 'https://api.olganews.students.nomoredomains.rocks/';
+export const  BASE_URL_AUTH = 'https://api.news.olgailyina.ru/';
 //9e67a2c23aab43618784d891cfc6970e
 // https://newsapi.org/v2/everything?
 // https://nomoreparties.co/news/
